f08134db5cbe6a7c464c869799ec9770e4b7403b
Josh Borrow <josh@joshborrow.com>
2023-10-09 11:45:26 -0400

    Added test to see if we can recover nodes

--- tests/test_quadtree.py
+++ tests/test_quadtree.py
@@ -19,3 +19,22 @@ def test_tree_build():
     tree.walk_tree_and_populate()
 
     assert tree.initialized
+
+
+def test_tree_recover():
+    config = TreeConfiguration(
+        base_grid_size=64,
+        refinement_levels=2,
+        dtype=np.float64,
+    )
+
+    tree = QuadTree(config)
+    base_array = np.random.rand(256, 256)
+    tree.initialize_from_array(base_array)
+    tree.walk_tree_and_populate()
+
+    recovered = tree.nodes[0][0].children[0].children[0].data
+
+    assert recovered.shape == (64, 64)
+    assert recovered.dtype == np.float64
+    assert (recovered == base_array[0:64, 0:64]).all()
